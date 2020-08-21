76ba91b67db876656a318e5baedb76a85fd8eb34
Graham Sysko <gsysko@gmail.com>
2020-08-21 12:59:20 +0100

    Stashing override work

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import CoreAssistant from '@sketch-hq/sketch-core-assistant'
 import { AssistantPackage, RuleDefinition } from '@sketch-hq/sketch-assistant-types'
 
+//Pages Required Rule
 const pageNaming: RuleDefinition = {
   rule: async (context) => {
     const { utils } = context
@@ -45,12 +46,56 @@ const pageNaming: RuleDefinition = {
     'Checkst the following required pages are present: ⚛️ symbols, 💁‍♂️‍ component overview.',
 }
 
+//Override naming rule
+const overrideNaming: RuleDefinition = {
+  rule: async (context) => {
+    const { utils } = context
+    function assertOption(value: unknown): asserts value is string[] {
+      if (!Array.isArray(value)) {
+        throw new Error('Option value is not an array')
+      }
+      for (let i = 0; i < value.length; i++) {
+        if (typeof value[i] !== 'string') {
+          throw new Error('Option array element is not a string')
+        }
+      }
+    }
+
+    // Get a configuration option named "required"
+    const iconOverride = utils.getOption('iconOverride')
+    assertOption(iconOverride)
+
+    //Check icon overrides
+    for (const requirement of iconOverride) {
+      // var found = false
+      // Iterate
+      for (const symbol of utils.objects.symbolMaster) {
+        for const override of symbol.overrides
+        throw new Error(symbol.ov + requirement)
+        // Test
+        // if (value.includes(requirement)) {
+        //   // Report
+        //   found = true
+        // }
+      }
+
+      //Report
+      // if (!found) {
+      //   utils.report(`Page “${requirement}” not found.`)
+      // }
+    }
+  },
+  name: 'conversation-design-system/name-pattern-overrides',
+  title: 'Override not named correctly',
+  description: '.....',
+}
+
 const assistant: AssistantPackage = [
   CoreAssistant,
   async () => {
     return {
       name: 'conversation-design-system',
-      rules: [pageNaming],
+      rules: [pageNaming, overrideNaming],
       config: {
         rules: {
           'conversation-design-system/name-required-pages': {
@@ -68,6 +113,10 @@ const assistant: AssistantPackage = [
             ],
             forbidden: [],
             ruleTitle: 'Non-standard page name found',
+          },
+          'conversation-design-system/name-pattern-overrides': {
+            active: true,
+            iconOverride: ['⚡️', '↪️⚡️'],
           },
         },
       },
