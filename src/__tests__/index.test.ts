167e5d7d2c6682afd413e9d33843da0ee7b157b6
Graham Sysko <gsysko@gmail.com>
2020-09-09 19:05:47 +0100

    Enforce NO space after emoji

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -11,28 +11,28 @@ test('test assistant', async () => {
   expect(violations[0].message).toBe('Layer name does not match any of the allowed patterns')
   expect(violations[1].message).toBe('Page “💁‍♂️ component overview” not found.')
   expect(violations[2].message).toBe(
-    'Fill layer Rectangle (L) should be prefixed with "🎨" (followed by a space).',
+    'Fill layer Rectangle (L) should be prefixed with "🎨" (followed by no space).',
   )
   expect(violations[3].message).toBe(
-    'Text layer label (L) should be prefixed with "✏️"  (followed by a space).',
+    'Text layer label (L) should be prefixed with "✏️"  (followed by no space).',
   )
   expect(violations[4].message).toBe(
-    'Image layer pic (L) should be prefixed with "🖼" (followed by a space).',
+    'Image layer pic (L) should be prefixed with "🖼" (followed by no space).',
   )
   expect(violations[5].message).toBe(
-    'Icon layer ⚡️icon 2 (L) should be prefixed with "⚡️"  (followed by a space).',
+    'Icon layer ⚡️ icon 2 (L) should be prefixed with "⚡️"  (followed by no space).',
   )
   expect(violations[6].message).toBe(
-    'Image layer pic should be prefixed with "🖼" (followed by a space).',
+    'Image layer pic should be prefixed with "🖼" (followed by no space).',
   )
   expect(violations[7].message).toBe(
-    'Fill layer Rectangle should be prefixed with "🎨" (followed by a space).',
+    'Fill layer Rectangle should be prefixed with "🎨" (followed by no space).',
   )
   expect(violations[8].message).toBe(
-    'Icon layer ⚡️icon 2 should be prefixed with "⚡️"  (followed by a space).',
+    'Icon layer ⚡️ icon 2 should be prefixed with "⚡️"  (followed by no space).',
   )
   expect(violations[9].message).toBe(
-    'Text layer label should be prefixed with "✏️"  (followed by a space).',
+    'Text layer label should be prefixed with "✏️"  (followed by no space).',
   )
   expect(violations).toHaveLength(10)
   expect(ruleErrors).toHaveLength(0)
